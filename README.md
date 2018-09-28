5cdbebbad439182d0f9fd56e99063e690dbb5aa5
Andrew Cornies <acornies@tucows.com>
2018-09-28 15:30:17 -0400

    Initial import of grafana-annotate OpenFaaS function handler code and readme added
    Signed-off-by: Andrew Cornies <acornies@tucows.com>

---
+++ README.md
@@ -0,0 +1,47 @@
+# faas-grafana-annotate
+An OpenFaaS function to create annotations in Grafana (>= v4.6).
+
+## Supported Request Parameters
+- body (string, text/plain)
+- query string
+  - tag (string, multiple, optional)
+  - panelId (int, optional)
+  - dashboardId (int, optional)
+
+By default, if no tags are provided, the tag "global" is used for your annotation. In your dashboard, make sure *Annotations & Alerts* is enabled and and filtered by the appropriate tag.
+
+## Function Configuration
+- environment
+  - grafana_url
+- secrets
+  - grafana_api_token
+  - grafana_username
+  - grafana_password
+
+This function prioritizes **grafana_api_token** first, then falls back to basic auth provided by grafana_username and grafana_password.
+
+### Grafana Configuration
+1) Add new API key (User icon -> API Keys) w/ Editor role
+2) In your dashboard, click gear icon -> Annotations -> enable
+   > In your dashboard, make sure *Annotations & Alerts* is enabled and and filtered by the appropriate tag.
+
+## Deployment Examples
+faas-cli (from Docker)
+```bash
+faas-cli deploy --image acornies/grafana-annotate --env grafana_url=http://example:3000
+```
+faas-cli (from source)
+```bash
+faas-cli deploy -f ./grafana-annotate.yml --env grafana_url=http://example:3000
+```
+
+## Invoke Examples
+faas-cli
+```bash
+faas-cli invoke grafana-annotate --query tag=global --query tag=faas --query dashboardId=1 --query panelId=1 --gateway http://localhost:8080
+```
+curl
+```bash
+curl -XPOST -d 'test annotation' "http://localhost:8080/function/grafana-annotate?tag=global&tag=faas&tag=application"
+```
+<!-- [!][grafana screenshot]() -->
---
+++ grafana-annotate/handler.go
@@ -0,0 +1,133 @@
+package function
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+)
+
+func Handle(req []byte) string {
+
+	grafanaUrl := os.Getenv("grafana_url")
+	if len(grafanaUrl) == 0 {
+		grafanaUrl = "http://grafana:3000"
+	}
+
+	payload := createPayload(string(req), grafanaUrl)
+
+	payloadJson, _ := json.Marshal(payload)
+	url := fmt.Sprintf("%s/api/annotations", grafanaUrl)
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadJson))
+
+	token, tokenErr := getSecret("grafana_api_token")
+
+	var contextMessage string
+
+	// try to use grafana_api_token secret first, then basic auth
+	if tokenErr != nil {
+		contextMessage = fmt.Sprintf("Grafana credential error: API token not found. Using basic auth: %v", tokenErr)
+		username, userErr := getSecret("grafana_username")
+		password, pwdErr := getSecret("grafana_password")
+
+		if userErr != nil {
+			contextMessage += fmt.Sprintf("Grafana credential error: username not found. Using default... %v", userErr)
+			username = []byte(`admin`)
+		}
+
+		if pwdErr != nil {
+			contextMessage += fmt.Sprintf("Grafana credential error: password not found. Using default... %v", pwdErr)
+			password = []byte(`admin`)
+		}
+		request.SetBasicAuth(string(username), string(password))
+	} else {
+		contextMessage = "Grafana API token enabled. Using API token..."
+		request.Header.Add("Authorization", "Bearer "+string(token))
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(request)
+
+	if err != nil {
+		return fmt.Sprintf("Failed to create annotation %v", err)
+	} else {
+		responseBody, responseErr := ioutil.ReadAll(resp.Body)
+		if responseErr != nil {
+			contextMessage += fmt.Sprintf("%v", responseErr)
+		}
+		if resp.StatusCode != 200 {
+			errorResponse := map[string]interface{}{
+				"statusCode": resp.StatusCode,
+				"message":    fmt.Sprintf("Failed to post annotation with status: %v", resp.Status),
+				"context":    contextMessage,
+			}
+			errorJson, _ := json.Marshal(errorResponse)
+			return string(errorJson)
+		} else {
+			return string(responseBody)
+		}
+	}
+}
+
+func getSecret(secretName string) (secretBytes []byte, err error) {
+	// read from the openfaas secrets folder
+	secretBytes, err = ioutil.ReadFile("/var/openfaas/secrets/" + secretName)
+	return secretBytes, err
+}
+
+func createPayload(input string, queryString string) map[string]interface{} {
+
+	now := time.Now()
+	nanos := now.UnixNano()
+	millis := nanos / 1000000
+
+	payload := map[string]interface{}{}
+
+	params, paramsError := url.ParseQuery(os.Getenv("Http_Query"))
+	if paramsError != nil {
+		payload = map[string]interface{}{
+			"time":     millis,
+			"isRegion": false,
+			"timeEnd":  0,
+			"text":     input,
+			"tags":     []string{"global"},
+		}
+	} else {
+		payload = map[string]interface{}{
+			"time":     millis,
+			"isRegion": false,
+			"timeEnd":  0,
+			"text":     input,
+		}
+		if len(params["tag"]) > 0 {
+			newTags := []string{}
+			for _, tag := range params["tag"] {
+				newTags = append(newTags, tag)
+			}
+			payload["tags"] = newTags
+		} else {
+			payload["tags"] = []string{"global"}
+		}
+		if len(params["dashboardId"]) > 0 {
+			dashId, dashErr := strconv.Atoi(params["dashboardId"][0])
+			if dashErr == nil {
+				payload["dashboardId"] = dashId
+			}
+		}
+		if len(params["panelId"]) > 0 {
+			panelId, panelErr := strconv.Atoi(params["panelId"][0])
+			if panelErr == nil {
+				payload["panelId"] = panelId
+			}
+		}
+	}
+	return payload
+}
